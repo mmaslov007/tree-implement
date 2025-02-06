83797cf4d34a86bab68eac78becf76d46a7f6f87
mmaslov007 <maslov.maxwell@student.greenriver.edu>
2025-02-06 14:48:55 -0800

    Tree transversal methods added

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,40 @@
 public class Traversal {
   public static void main(String[] args) {
+    TreeNode root = new TreeNode(77, null, null);
+    root.left = new TreeNode(22, null, null);
+    root.right = new TreeNode(86, null, null);
+    root.left.right = new TreeNode(33, null, null);
 
+    postOrder(root);
+  }
+
+  public static void preOrder(TreeNode node) {
+    if (node == null) {
+      return;
+    }
+
+    System.out.println(node.value);
+    preOrder(node.left);
+    preOrder(node.right);
+  }
+
+  public static void postOrder(TreeNode node) {
+    if (node == null) {
+      return;
+    }
+
+    postOrder(node.left);
+    postOrder(node.right);
+    System.out.println(node.value);
+  }
+
+  public static void inOrder(TreeNode node) {
+    if (node == null) {
+      return;
+    }
+
+    inOrder(node.left);
+    System.out.println(node.value);
+    inOrder(node.right);
   }
 }
